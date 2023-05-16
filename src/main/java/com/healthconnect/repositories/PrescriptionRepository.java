7a82d60d70af3576c62ae641a4ac521b5b90c5e5
billyjeans <bilalahmed054@gmail.com>
2023-05-16 21:25:13 +0100

    Health-Connect-Master: added Spring data JPA repositories.

---
+++ src/main/java/com/healthconnect/repositories/PrescriptionRepository.java
@@ -0,0 +1,10 @@
+package com.healthconnect.repositories;
+
+import com.healthconnect.model.Prescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+}
+
