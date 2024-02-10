c06ecbf71e1d65a060b5fad8e20dea4f5fe0ab62
biah01 <bilal.ahmed@amova.eu>
2024-02-10 22:34:59 +0100

    Health-Connect: PersistenceConfiguration added for HealthConnect

--- src/main/java/com/healthconnect/configuration/PersistenceConfiguration.java
+++ src/main/java/com/healthconnect/configuration/PersistenceConfiguration.java
@@ -1,21 +1,68 @@
 package com.healthconnect.configuration;
 
 import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import java.util.Properties;
 
-@Configuration
 @EnableAutoConfiguration
+@EnableTransactionManagement
+
 public class PersistenceConfiguration {
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
 
     @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("com.healthconnect.model");
+        emf.setPersistenceUnitName("HealthConnect"); // Specify your persistence unit name here
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect")); // Load dialect from properties file
+        emf.setJpaProperties(properties);
+
+        return emf;
+    }
+
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         JpaTransactionManager tm = new JpaTransactionManager();
-        tm.setEntityManagerFactory(emf);
+        tm.setEntityManagerFactory(entityManagerFactory);
         return tm;
     }
 }
-
-
